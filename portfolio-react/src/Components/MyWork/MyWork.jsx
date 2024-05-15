7ebdec81e43016965157c7deecd175abee03cfeb
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-15 15:12:28 +0530

    add services

---
+++ portfolio-react/src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,9 @@
+import theme_pattern from "../../assets/theme_pattern.svg";
+import React from "react";
+import "./MyWork.css";
+
+function MyWork() {
+  return <div>MyWork</div>;
+}
+
+export default MyWork;
