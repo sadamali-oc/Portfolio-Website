fc7dd3f81aac4536ccdea50cbe082f0348446d00
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-05-15 15:19:21 +0530

    Update MyWork.jsx

--- portfolio-react/src/Components/MyWork/MyWork.jsx
+++ portfolio-react/src/Components/MyWork/MyWork.jsx
@@ -3,7 +3,15 @@ import React from "react";
 import "./MyWork.css";
 
 function MyWork() {
-  return <div>MyWork</div>;
+  return <div className="mywork">
+    <div className="mywork-title">
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt=""/>
+    </div>
+    <div className="mywork-container">
+        
+    </div>
+  </div>;
 }
 
 export default MyWork;
